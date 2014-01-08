16662b6f8f170c585471909172758f69b871203c
John Gibbons <jgibbons@bonanzapress.com>
2014-01-08 14:28:28 -0800

    Added full title helper.

--- Gemfile
+++ Gemfile
@@ -7,11 +7,16 @@ gem 'rails', '4.0.2'
 group :development, :test do
   gem 'mysql2'
   gem 'rspec-rails', '2.13.1'
+  #gem 'guard-rspec', '2.5.0'
 end
 
 group :test do
   gem 'selenium-webdriver', '2.35.1'
   gem 'capybara', '2.1.0'
+
+  #gem 'rb-notifu', '0.0.4'
+  #gem 'win32console', '1.3.2'
+  #gem 'wdm', '0.1.0'
 end
 
 gem 'sass-rails', '4.0.1'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,9 +7,14 @@ describe "StaticPages" do
       expect(page).to have_content('Sample App')
     end
 
-    it "should have the correct title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
@@ -26,7 +31,6 @@ describe "StaticPages" do
     end
   end
 
-
   describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
@@ -36,6 +40,18 @@ describe "StaticPages" do
     it "should have the correct title 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the correct title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
     end
   end
 end
